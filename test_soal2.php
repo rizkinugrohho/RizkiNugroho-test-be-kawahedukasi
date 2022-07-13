1f288559d6d2abb8200f432e9d4e033c85dd8688
Rizki Nugroho <36374356+rizkinugrohho@users.noreply.github.com>
2022-07-13 10:46:39 +0700

    Rizki Nugroho : Test Kawah Edukasi Batch III

--- test_soal2.php
+++ test_soal2.php
@@ -5,7 +5,7 @@ for($i =0;$i<count($data);$i++){
 if($i <0){
 	echo("Tidak bisa di validasi sistem <br>");
 }elseif(is_string($data[$i])){
-	echo("index ke $i adalah string dengan data $data[$i] <br>");
+	echo("index ke $i adalah string dengan data $data[$i]<br>");
 }elseif(is_int($data[$i])){
 	echo("index ke $i adalah integer dengan data $data[$i]<br>");
 }
