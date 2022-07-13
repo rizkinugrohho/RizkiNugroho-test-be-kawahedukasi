0dced9d044dc7e4eff29cbcc750f73ca47ad9bcf
Rizki Nugroho <36374356+rizkinugrohho@users.noreply.github.com>
2022-07-13 10:45:51 +0700

    Update test_soal2.php

--- test_soal2.php
+++ test_soal2.php
@@ -1,12 +1,12 @@
 <?php
   $data = [1, "ka", 67, "wah", "1772", "edukasi", 7, 98, -1];
 
-for($i =0;$x<count($data);$i++){
+for($i =0;$i<count($data);$i++){
 if($i <0){
 	echo("Tidak bisa di validasi sistem <br>");
 }elseif(is_string($data[$i])){
 	echo("index ke $i adalah string dengan data $data[$i] <br>");
-}elseif(is_int($data[$x])){
+}elseif(is_int($data[$i])){
 	echo("index ke $i adalah integer dengan data $data[$i]<br>");
 }
 }
